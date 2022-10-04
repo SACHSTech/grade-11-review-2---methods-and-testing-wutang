1a4d6fdce2b5c549345193c4b51972cc798aaf79
NathanWan23 <Nathan.wan23@ycdsbk12.ca>
2022-10-03 21:34:45 -0400

    Basically done

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,8 +1,203 @@
 package gr11review.part2;
+import java.io.*;
+import java.util.*;
 
+/**
+* Where all the methods are located. 
+* @author: N. Wan 
+*
+*/
 public class Utility {
 
-    
+    /** Creates an employee with the specified name.
+    * @param lastName The employee’s last name.
+    * @return boolean The 
+    */
+    public static boolean xyzMiddle(String str){ 
 
+        // Initialize variables
+        int intLength = str.length();
+        int intSides;
+        String strTester;
+        String strChecker = "xyz";
+
+        // Determine if it is atleast 5 characters long
+        if (intLength >= 5) {
+
+            intSides = intLength - 3;
     
+            // Check if it is even or odd then check if "xyz" is in the middle.
+            if (intSides % 2 != 0) {
+                
+                intSides = intSides / 2;
+    
+                strTester = str.substring(intSides, intSides + 3);
+
+                if (strTester.equals(strChecker) == true) {
+                    
+                    return true;
+
+                } else {
+
+                    strTester = str.substring(intSides +1, intSides + 4);
+
+                    if (strTester.equals(strChecker) == true) {
+                        
+                        return true;
+
+                    }
+                }
+
+            // Check if "xyz" is in the middle for even. 
+            } else {
+
+                intSides = intSides / 2;
+
+                strTester = str.substring(intSides, intSides + 3);
+
+                if (strTester.equals(strChecker) == true) {
+                  
+                    return true;
+                
+                } 
+            }
+    }
+
+    return false;
+
+    }
+
+    /** A method that finds the alphabetically first word and returns it. 
+    * @param filenametxt The file that contains all the information needed.
+    * @return highestWord The word that is most alphabetically first.
+    * @IOExecption 
+    */
+    public static String alphaWord(String filenametxt) throws IOException { 
+     
+        // Initialize buffer reader and variables.
+        BufferedReader Br = new BufferedReader(new FileReader(filenametxt));
+        String strLine = Br.readLine();
+        String highestWord = Br.readLine();
+
+    // Read the lines in the file and compare them to see which word is the alphabetically first word. 
+    while(strLine != null) {
+        
+        strLine = Br.readLine();
+
+        if (strLine.compareTo(highestWord) > 0) {
+
+            highestWord = strLine;
+
+        }
+    }
+
+    Br.close();
+
+    // Return the alphabetically first word. 
+    return highestWord;
+
+    }
+
+    /** A method that puts all the zeros in an array to the front. 
+    * @param nums The array that has the zeros in random position.
+    * @return zeroFront The array that has all the zeros at the front of the array.
+    */
+    public static int[] zeroFront(int[] nums) {
+
+        // Initialize variables
+        int intZeroCounter = 0;
+        int[] zeroFront;
+        zeroFront = new int[nums.length];
+        int intCounter = 0;
+
+        // Count how many zeros there are
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0) {
+
+                intZeroCounter++;
+                
+            } 
+        }
+
+        // Add the non-zero numbers to the end.
+        for(int i = 0; i < nums.length; i++){
+
+            if (nums[i] != 0) {
+
+                zeroFront[intZeroCounter + intCounter] = nums[i];
+                intCounter++;
+            }
+        }
+
+        // Add the zeros to the front.
+        for(int i = 0; i < intZeroCounter; i++) {
+
+            zeroFront[i] = 0;
+        }
+
+        // Return array
+        return zeroFront;   
+    }
+
+    /** A method that creates an array with an ascending pattern. 
+    * @param n The highest number.
+    * @return seriesUp An array with an ascending pattern.
+    */
+    public static int[] seriesUp(int n) {
+
+        // Initialize variables 
+        int[] seriesUp;
+        int intLength = n * (n+1) / 2;
+        seriesUp = new int[intLength];
+        int intCounter = 1;
+        int intPosition = 0;
+        int intEnd = -1;
+
+        // Create the ascending pattern array.
+        for (int i = 1; i <= n; i++) {
+
+            intEnd = intEnd + i;
+
+            while (intPosition <= intEnd) {
+
+                seriesUp[intPosition] = intCounter;
+
+                intCounter++;
+
+                intPosition++;
+
+            }
+
+            intCounter = 1;
+        }
+
+        // Return the array.
+        return seriesUp;
+
+    }
+
+    /** A method that takes a portion of a 2D array and outputs it based on the rows and columns given. 
+    * @param arr The original 2D array. 
+    * @param row The row the portion goes up to.
+    * @param col The col the portion goes up to.
+    * @return ans The portion of the 2D array.
+    */
+    public static int[][] split(int[][] arr, int row, int col) {
+        
+        // Initialize protion of 2D array. 
+        int[][] ans = new int[row + 1][col + 1];
+
+        // Construct the portion of the 2D array.
+        for(int i = 0; i <= col; i++) {
+            for(int j = 0; j <= row; j++) {
+
+                ans[j][i] = arr[j][i];
+
+            }
+        }
+
+        // Return array. 
+        return ans;
+    }
 }
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -2,9 +2,164 @@ package gr11review.part2;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 import gr11review.part2.Utility;
 
 public class UtilityTest{
-  
+
+    // Initialize variables for zeroFront method test
+    int[] zeroFrontInput1 = {1,0,0,1};
+    int[] zeroFrontInput2 = {0,1,1,0,1};
+    int[] zeroFrontInput3 = {1,0};
+    int[] zeroFrontInput4 = {0,1,1,0,1};
+    int[] zeroFrontAns1 = {0,0,1,1};
+    int[] zeroFrontAns2 = {1,0,1,0,1};
+    int[] zeroFrontAns3 = {0,1};
+    int[] zeroFrontAns4 = {0,0,1,1,1};
+
+    // Initialize variables for seriesUp method test
+    int[] seriesUpAns1 = {1,1,2,1,2,3};
+    int[] seriesUpAns2 = {1,1,2,1,2,3,1,2,3,4};
+    int[] seriesUpAns3 = {1,1,2};
+    int[] seriesUpAns4 = {1,1,2,1,2,3,1,2,3,4,1,2,3,4,5,1,2,3,4,5,6,1,2,3,4,5,6,7,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,10};
+
+    // Initialize variables for split method test
+    int[][] splitInput = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int[][] splitAns1 = {
+        {1,2},
+        {4,5}
+    };
+    int[][] splitAns2 = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int[][] splitInput2 = {
+        {3,1,4,1,5},
+        {9,2,6,5,3},
+        {6,5,3,5,8},
+        {9,7,9,3,2},
+        {3,8,4,6,2}
+    };
+    int[][] splitAns3 = {
+        {3,1,4},
+        {9,2,6},
+        {6,5,3}
+    };
+    int[][] splitAns4 = {
+        {3,1,4},
+        {9,2,6},
+        {6,5,3},
+        {9,7,9}
+    };
+
+    // Tests for xyzMiddle
+    @Test
+    void testSignature(){
+        assertEquals(true, Utility.xyzMiddle("AAxyzBB"));
+    }
+
+    @Test
+    void testSignature1(){
+        assertEquals(true, Utility.xyzMiddle("AxyzBB"));
+    }
+
+    @Test
+    void testSignature2(){
+        assertEquals(false, Utility.xyzMiddle("AxyzBBB"));
+    }
+
+    @Test
+    void testSignature3(){
+        assertEquals(true, Utility.xyzMiddle("AAxyzB"));
+    }
+
+    // Tests for alphaWord
+    @Test
+    void testSignature4() throws IOException {
+        assertEquals("accompany", Utility.alphaWord("C:\\Users\\nw556\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\test2\\Review2_3Test_1.txt"));
+    }
+
+    @Test
+    void testSignature5() throws IOException {
+        assertEquals("x", Utility.alphaWord("C:\\Users\\nw556\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\test2\\Review2_3Test_2.txt"));
+    }
+
+    @Test
+    void testSignature6() throws IOException {
+        assertEquals("accompanyx", Utility.alphaWord("C:\\Users\\nw556\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\test2\\Review2_3Test_3.txt"));
+    }
+
+    @Test
+    void testSignature7() throws IOException {
+        assertEquals("accompany", Utility.alphaWord("C:\\Users\\nw556\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\test2\\Review2_3Test_4.txt"));
+    } 
+
+    // Tests for zeroFront
+    @Test
+    void testSignature8(){
+        assertArrayEquals(zeroFrontAns1, Utility.zeroFront(zeroFrontInput1));
+    }
+
+    @Test
+    void testSignature9(){
+        assertArrayEquals(zeroFrontAns2, Utility.zeroFront(zeroFrontInput2));
+    }
+
+    @Test
+    void testSignature10(){
+        assertArrayEquals(zeroFrontAns3, Utility.zeroFront(zeroFrontInput3));
+    }
+
+    @Test
+    void testSignature11(){
+        assertArrayEquals(zeroFrontAns4, Utility.zeroFront(zeroFrontInput4));
+    }
+
+    // Tests for seriesUp
+    @Test
+    void testSignature12(){
+        assertArrayEquals(seriesUpAns1, Utility.seriesUp(3));
+    }
+
+    @Test
+    void testSignature13(){
+        assertArrayEquals(seriesUpAns2, Utility.seriesUp(4));
+    }
+
+    @Test
+    void testSignature14(){
+        assertArrayEquals(seriesUpAns3, Utility.seriesUp(2));
+    }
+
+    @Test
+    void testSignature15(){
+        assertArrayEquals(seriesUpAns4, Utility.seriesUp(10));
+    }
+
+    // Tests for split
+    @Test
+    void testSignature16(){
+        assertArrayEquals(splitAns1, Utility.split(splitInput, 1, 1));
+    }
+    
+    @Test
+    void testSignature17(){
+        assertArrayEquals(splitAns2, Utility.split(splitInput, 2, 2));
+    }
+
+    @Test
+    void testSignature18(){
+        assertArrayEquals(splitAns3, Utility.split(splitInput2, 2, 2));
+    }
+
+    @Test
+    void testSignature19(){
+        assertArrayEquals(splitAns4, Utility.split(splitInput2, 3, 2));
+    }
 }

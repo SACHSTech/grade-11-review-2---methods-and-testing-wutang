e9d25a6bb40fb70a95c25375075761ed41f08550
jenusan <jenusan.yogarajah23@ycdsbk12.ca>
2022-10-03 23:45:35 -0400

    pull this

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -3,19 +3,229 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.*;
+import java.util.*;
 
+/**
+* Where all the methods are located. 
+* @author: N. Wan 
+*
+*/
 public class Utility {
 
+
+    /** A method that checks if "xyz" is in the middle of the string.
+    * @author N. Wan
+    * @param str The string that is going to be checked if there is "xyz" in the middle.
+    * @return boolean If "xyz" is in the middle or not. 
+    */
+    public static boolean xyzMiddle(String str){ 
+
+        // Initialize variables
+        int intLength = str.length();
+        int intSides;
+        String strTester;
+        String strChecker = "xyz";
+
+        // Determine if it is atleast 5 characters long
+        if (intLength >= 5) {
+
+            intSides = intLength - 3;
+            
+            // Check if it is even or odd then check if "xyz" is in the middle.
+            if (intSides % 2 != 0) {
+                
+                intSides = intSides / 2;
     
-    /**
+                strTester = str.substring(intSides, intSides + 3);
+
+                if (strTester.equals(strChecker) == true) {
+                    
+                    return true;
+
+                } else {
+
+                    strTester = str.substring(intSides +1, intSides + 4);
+
+                    if (strTester.equals(strChecker) == true) {
+                        
+                        return true;
+
+                    }
+                }
+
+            // Check if "xyz" is in the middle for even. 
+            } else {
+
+                intSides = intSides / 2;
+
+                strTester = str.substring(intSides, intSides + 3);
+
+                if (strTester.equals(strChecker) == true) {
+                  
+                    return true;
+                
+                } 
+            }
+    }
+
+    return false;
+
+    }
+
+
+    /** A method that finds the alphabetically first word and returns it. 
+    * @author N. Wan
+    * @param filenametxt The file that contains all the information needed.
+    * @return highestWord The word that is most alphabetically first.
+    * @IOExecption 
+    */
+    public static String alphaWord(String filenametxt) throws IOException { 
+     
+        // Initialize buffer reader and variables.
+        BufferedReader Br = new BufferedReader(new FileReader(filenametxt));
+        String strLine = Br.readLine();
+        String highestWord = Br.readLine();
+
+    // Read the lines in the file and compare them to see which word is the alphabetically first word. 
+    while(strLine != null) {
+        
+        strLine = Br.readLine();
+
+        if (strLine.compareTo(highestWord) > 0) {
+
+            highestWord = strLine;
+
+        }
+    }
+
+    Br.close();
+
+    // Return the alphabetically first word. 
+    return highestWord;
+    
+    }
+
+
+    /** A method that puts all the zeros in an array to the front. 
+    * @param nums The array that has the zeros in random position.
+    * @return zeroFront The array that has all the zeros at the front of the array.
+    */
+    public static int[] zeroFront(int[] nums) {
+
+        // Initialize variables
+        int intZeroCounter = 0;
+        int[] zeroFront;
+        zeroFront = new int[nums.length];
+        int intCounter = 0;
+
+        // Count how many zeros there are
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 0) {
+
+                intZeroCounter++;
+                
+            } 
+        }
+
+        // Add the non-zero numbers to the end.
+        for(int i = 0; i < nums.length; i++){
+
+            if (nums[i] != 0) {
+
+                zeroFront[intZeroCounter + intCounter] = nums[i];
+                intCounter++;
+            }
+        }
+
+        // Add the zeros to the front.
+        for(int i = 0; i < intZeroCounter; i++) {
+
+            zeroFront[i] = 0;
+        }
+
+        // Return array
+        return zeroFront;   
+    }
+
+
+    /** A method that creates an array with an ascending pattern. 
+    * @param n The highest number.
+    * @return seriesUp An array with an ascending pattern.
+    */
+    public static int[] seriesUp(int n) {
+
+        // Initialize variables 
+        int[] seriesUp;
+        int intLength = n * (n+1) / 2;
+        seriesUp = new int[intLength];
+        int intCounter = 1;
+        int intPosition = 0;
+        int intEnd = -1;
+
+        // Create the ascending pattern array.
+        for (int i = 1; i <= n; i++) {
+
+            intEnd = intEnd + i;
+
+            while (intPosition <= intEnd) {
+
+                seriesUp[intPosition] = intCounter;
+
+                intCounter++;
+
+                intPosition++;
+
+            }
+
+            intCounter = 1;
+        }
+
+        // Return the array.
+        return seriesUp;
+
+    }
+
+
+    /** A method that takes a portion of a 2D array and outputs it based on the rows and columns given. 
+    * @param arr The original 2D array. 
+    * @param row The row the portion goes up to.
+    * @param col The col the portion goes up to.
+    * @return ans The portion of the 2D array.
+    */
+    public static int[][] split(int[][] arr, int row, int col) {
+        
+        // Initialize protion of 2D array. 
+        int[][] ans = new int[row + 1][col + 1];
+
+        // Construct the portion of the 2D array.
+        for(int i = 0; i <= col; i++) {
+            for(int j = 0; j <= row; j++) {
+
+                ans[j][i] = arr[j][i];
+
+            }
+        }
+
+        // Return array. 
+        return ans;
+        
+    }
+
+
+/**
      * method to check if a string contains characters ys that balance character xs
+     * @author J. Yogarajah
      * @param str inputs a string that contains letters 
      * @return boolean vlaue on if the string xs are balanced 
      */
     public static boolean xyBalance(String str){
+        // intializing variables
         int Lengthstyll = str.length();
         boolean YTrue = false;
 
+        // for loop to check contents
         for (int i = 0; i < Lengthstyll; i++){
             char chrletter = str.charAt(i);
             if (chrletter == 'y'){
@@ -28,6 +238,7 @@ public class Utility {
             }
         }
 
+        // return statements based on outcomes
         if (!YTrue){
             return true;
         }else{
@@ -35,39 +246,47 @@ public class Utility {
         }
     }
 
-    
+
     /**
      * method to return the longest word in a txt file
+     * @author J. Yogarajah
      * @param filenametxt enters files from the same directory
      * @return longest string in the file 
      * @throws FileNotFoundException
      */
     public static String longestWord(String filenametxt) throws FileNotFoundException{
+        // initializing variables 
         String strlongest = "";
         String strCurrent;
 
+        // scanner to read file (buffered reader sucks)
         Scanner sc = new Scanner(new File(filenametxt));
 
+        // while loop to find longest word
         while (sc.hasNext()) {
             strCurrent = sc.next();
              if (strCurrent.length() > strlongest.length()) {
                 strlongest = strCurrent;
              }
     }
+    // return longst word 
     return strlongest;    
 }
 
 
 /**
  * method that enters an array and returns one which contains no 10s, and instead 0s at the end
+ * @author J. Yogarajah
  * @param nums enters an array of numbers to be operated on 
  * @return the same except without the 10s found inside 
  */
 public static int[] withoutTen(int[] nums){
 
+    // initilize variables
     int[] newarr = new int[nums.length];
     int x = 0;
 
+    // for loop to operate on array 
     for (int i = 0; i < nums.length; i++){
         if (nums[i] != 10){
             newarr[i - x] = nums[i];
@@ -75,30 +294,36 @@ public static int[] withoutTen(int[] nums){
             x++;
         }
     }
+    // return new array 
     return newarr;
 }
 
 
 /**
  * method to determine wether there is a point where the array can equally be split to weigh the same on both sides 
+ * @author J. Yogarajah
  * @param nums array input of numbers
  * @return boolean on wether there is a point where the array can equally be split to weigh the same on both sides 
  */
 public static boolean canBalance(int[] nums){
+    // initilize variables
     int sum = 0;
     int half = 0;
     int splitsum = 0;
     int i=0;
 
 
+    // if satement to kill odd numbers 
     if (nums.length == 0){
         return false;
     }
 
+    // operation to find sum of array contents
     for (int x = 0; x < nums.length; x++){
         sum = sum + nums[x];
     }
 
+    // system to check if array can be balanced
     if (sum%2 ==0){
       half = sum / 2;
         while(splitsum != half){
@@ -112,6 +337,7 @@ public static boolean canBalance(int[] nums){
         return false;
     }
 
+    // return statements 
     if (splitsum == half){
       return true;
     }else{
@@ -122,15 +348,18 @@ public static boolean canBalance(int[] nums){
 
 /**
  * method to reverse a 2d array 
+ * @author J. Yogarajah
  * @param arr inputs a 2d array off numbers
  * @return 2d arrray of the original array reversed 
  */
 public static int[][] reverse(int[][] arr){
 
+    // initializing variables
     int[][] newarr = new int[arr.length][arr[arr.length - 1].length];
           int y = 0;
           int w = 0;
           
+          // for loops to operate on the arrays 
           for (int i = arr.length - 1; i >= 0; i--){
             for (int x = arr[arr.length - 1].length - 1; x >= 0; x--){
                 newarr[y][w] = arr[i][x];
@@ -140,6 +369,7 @@ public static int[][] reverse(int[][] arr){
             y++;
           }
 
+          // returns the array 
           return newarr;
 }
 
--- src/gr11review/part2/UtilityTest.java
+++ src/gr11review/part2/UtilityTest.java
@@ -4,14 +4,247 @@ import org.junit.jupiter.api.Test;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import org.junit.jupiter.api.TestInstance;
 
 
 public class UtilityTest{
 
+    // Initialize variables for zeroFront method test
+    int[] zeroFrontInput1 = {1,0,0,1};
+    int[] zeroFrontInput2 = {0,1,1,0,1};
+    int[] zeroFrontInput3 = {1,0};
+    int[] zeroFrontInput4 = {0,1,1,0,1};
+    int[] zeroFrontAns1 = {0,0,1,1};
+    int[] zeroFrontAns2 = {0,0,1,1,1};
+    int[] zeroFrontAns3 = {0,1};
+    int[] zeroFrontAns4 = {0,0,1,1,1};
+
+    // Initialize variables for seriesUp method test
+    int[] seriesUpAns1 = {1,1,2,1,2,3};
+    int[] seriesUpAns2 = {1,1,2,1,2,3,1,2,3,4};
+    int[] seriesUpAns3 = {1,1,2};
+    int[] seriesUpAns4 = {1,1,2,1,2,3,1,2,3,4,1,2,3,4,5,1,2,3,4,5,6,1,2,3,4,5,6,7,1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8,9,1,2,3,4,5,6,7,8,9,10};
+
+    // Initialize variables for split method test
+    int[][] splitInput = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int[][] splitAns1 = {
+        {1,2},
+        {4,5}
+    };
+    int[][] splitAns2 = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+    int[][] splitInput2 = {
+        {3,1,4,1,5},
+        {9,2,6,5,3},
+        {6,5,3,5,8},
+        {9,7,9,3,2},
+        {3,8,4,6,2}
+    };
+    int[][] splitAns3 = {
+        {3,1,4},
+        {9,2,6},
+        {6,5,3}
+    };
+    int[][] splitAns4 = {
+        {3,1,4},
+        {9,2,6},
+        {6,5,3},
+        {9,7,9}
+    };
+
+    
+    @Test
+    /**
+     * Test for xyzMiddle
+     * @author N. Wan
+     */
+    void testSignature(){
+        assertEquals(true, Utility.xyzMiddle("AAxyzBB"));
+    }
+
+    @Test
+    /**
+     * Test for xyzMiddle
+     * @author N. Wan
+     */
+    void testSignature1(){
+        assertEquals(true, Utility.xyzMiddle("AxyzBB"));
+    }
+
+    @Test
+    /**
+     * Test for xyzMiddle
+     * @author N. Wan
+     */
+    void testSignature2(){
+        assertEquals(false, Utility.xyzMiddle("AxyzBBB"));
+    }
+
+    @Test
+    /**
+     * Test for xyzMiddle
+     * @author N. Wan
+     */
+    void testSignature3(){
+        assertEquals(true, Utility.xyzMiddle("AAxyzB"));
+    }
+
+    @Test
+    /**
+     * Test for alphaWord
+     * @author N. Wan
+     */
+    void testSignature4() throws IOException {
+        assertEquals("accompany", Utility.alphaWord(("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_1.txt")));
+    }
+    
+
+    @Test
+    /**
+     * Test for alphaWord
+     * @author N. Wan
+     */
+    void testSignature5() throws IOException {
+        assertEquals("x", Utility.alphaWord(("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_1.txt")));
+    }
+
+    @Test
+    /**
+     * Test for alphaWord
+     * @author N. Wan
+     */
+    void testSignature6() throws IOException {
+        assertEquals("accompanyx", Utility.alphaWord(("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_1.txt")));
+    }
+
+    @Test
+    /**
+     * Test for alphaWord
+     * @author N. Wan
+     */
+    void testSignature7() throws IOException {
+        assertEquals("accompany", Utility.alphaWord(("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_1.txt")));
+    } 
+
+    @Test
+    /**
+     * Test for zeroFront
+     * @author N. Wan
+     */
+    void testSignature8(){
+        assertArrayEquals(zeroFrontAns1, Utility.zeroFront(zeroFrontInput1));
+    }
+
+    @Test
+    /**
+     * Test for zeroFront
+     * @author N. Wan
+     */
+    void testSignature9(){
+        assertArrayEquals(zeroFrontAns2, Utility.zeroFront(zeroFrontInput2));
+    }
+
+    @Test
+    /**
+     * Test for zeroFront
+     * @author N. Wan
+     */
+    void testSignature10(){
+        assertArrayEquals(zeroFrontAns3, Utility.zeroFront(zeroFrontInput3));
+    }
+
+    @Test
+    /**
+     * Test for zeroFront
+     * @author N. Wan
+     */
+    void testSignature11(){
+        assertArrayEquals(zeroFrontAns4, Utility.zeroFront(zeroFrontInput4));
+    }
+
+    @Test
+    /**
+     * Test for seriesUp
+     * @author N. Wan
+     */
+    void testSignature12(){
+        assertArrayEquals(seriesUpAns1, Utility.seriesUp(3));
+    }
+
+    @Test
+    /**
+     * Test for seriesUp
+     * @author N. Wan
+     */
+    void testSignature13(){
+        assertArrayEquals(seriesUpAns2, Utility.seriesUp(4));
+    }
+
+    @Test
+    /**
+     * Test for seriesUp
+     * @author N. Wan
+     */
+    void testSignature14(){
+        assertArrayEquals(seriesUpAns3, Utility.seriesUp(2));
+    }
+
+    @Test
+    /**
+     * Test for seriesUp
+     * @author N. Wan
+     */
+    void testSignature15(){
+        assertArrayEquals(seriesUpAns4, Utility.seriesUp(10));
+    }
+
+    @Test
+    /**
+     * Test for split
+     * @author N. Wan
+     */
+    void testSignature16(){
+        assertArrayEquals(splitAns1, Utility.split(splitInput, 1, 1));
+    }
+    
+    @Test
+    /**
+     * Test for split
+     * @author N. Wan
+     */
+    void testSignature17(){
+        assertArrayEquals(splitAns2, Utility.split(splitInput, 2, 2));
+    }
+
+    @Test
+    /**
+     * Test for split
+     * @author N. Wan
+     */
+    void testSignature18(){
+        assertArrayEquals(splitAns3, Utility.split(splitInput2, 2, 2));
+    }
+
+    @Test
+    /**
+     * Test for split
+     * @author N. Wan
+     */
+    void testSignature19(){
+        assertArrayEquals(splitAns4, Utility.split(splitInput2, 3, 2));
+    }
+
     @Test 
     /**
      * Test for canBalance method
+     * @author J. Yogarajah
      */
     void canBalanceTest1(){
         int[] arr = {10,10};
@@ -20,6 +253,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for canBalance method
+     * @author J. Yogarajah
      */
     void canBalanceTest2(){
         int[] arr = {5,10,15};
@@ -29,6 +263,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for canBalance method
+     * @author J. Yogarajah
      */
     void canBalanceTest3(){
         int[] arr = {1,15,16};
@@ -37,6 +272,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for canBalance method
+     * @author J. Yogarajah
      */
     void canBalanceTest4(){
         int[] arr = {10,11};
@@ -48,6 +284,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for xyBalance method
+     * @author J. Yogarajah
      */
     void xyBalanceTest1(){
         assertTrue(Utility.xyBalance("fbbfkdsfjdfjnsdjkfyx"));
@@ -55,6 +292,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for xyBalance method
+     * @author J. Yogarajah
      */
     void xyBalanceTest2(){
         assertTrue(Utility.xyBalance("fbbfkdsfjdfjnsdjkfyxxxx"));
@@ -62,12 +300,14 @@ public class UtilityTest{
     @Test 
     /**
      * Test for xyBalance method
+     * @author J. Yogarajah
      */
     void xyBalanceTest3(){
         assertTrue(Utility.xyBalance("fbbfkydsfjdfjnsdjkfdsfdsx"));
     }
     /**
      * Test for xyBalance method
+     * @author J. Yogarajah
      */
     @Test 
     void xyBalanceTest4(){
@@ -76,6 +316,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for xyBalance method
+     * @author J. Yogarajah
      */
     void xyBalanceTest5(){
         assertTrue(Utility.xyBalance("fbbfkdsfjdfjnsdjkf"));
@@ -86,6 +327,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for withoutTen method
+     * @author J. Yogarajah
      */
     void withoutTenTest1(){
         int[] arr = {10,1,5};
@@ -95,6 +337,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for withoutTen method
+     * @author J. Yogarajah
      */
     void withoutTenTest2(){
         int[] arr = {10,10,10};
@@ -104,6 +347,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for withoutTen method
+     * @author J. Yogarajah
      */
     void withoutTenTest3(){
         int[] arr = {5,0,5};
@@ -113,6 +357,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for withoutTen method
+     * @author J. Yogarajah
      */
     void withoutTenTest4(){
         int[] arr = {0,0,0};
@@ -125,6 +370,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for reverse method
+     * @author J. Yogarajah
      */
     void reverseTest1(){
         int[][] arr = {{1,2,3}, {4,5,6}, {7,8,9}};
@@ -134,6 +380,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for reverse method
+     * @author J. Yogarajah
      */
     void reverseTest2(){
         int[][] arr = {{1,2,3}, {1,2,3}, {1,2,3}};
@@ -143,6 +390,7 @@ public class UtilityTest{
     @Test 
     /**
      * Test for reverse method
+     * @author J. Yogarajah
      */
     void reverseTest3(){
         int[][] arr = {{10,20,30}, {2,4,6}, {8,16,32}};
@@ -153,6 +401,7 @@ public class UtilityTest{
     @Test
     /**
      * Test for longestword method
+     * @author J. Yogarajah
      */
     void longestwordTest1() throws IOException{
         assertEquals("conservation", Utility.longestWord("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_1.txt"));
@@ -160,6 +409,7 @@ public class UtilityTest{
     @Test
     /**
      * Test for longestword method
+     * @author J. Yogarajah
      */
     void longestwordTes2() throws IOException{
         assertEquals("x", Utility.longestWord("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_2.txt"));
@@ -167,15 +417,18 @@ public class UtilityTest{
     @Test
     /**
      * Test for longestword method
+     * @author J. Yogarajah
      */
     void longestwordTes3() throws IOException{
-        assertEquals("conservation", Utility.longestWord("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_3.txt"));
+        assertEquals("conservation", Utility.longestWord(("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_3.txt")));
     }
     @Test
     /**
      * Test for longestword method
+     * @author J. Yogarajah
      */
     void longestwordTes4() throws IOException{
         assertEquals("conservation", Utility.longestWord("C:\\Users\\Jenus\\github-classroom\\SACHSTech\\grade-11-review-2---methods-and-testing-wutang\\src\\gr11review\\part2\\Review2_3Test_4.txt"));
+
     }
 }
